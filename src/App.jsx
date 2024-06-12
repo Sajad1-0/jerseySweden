7e55f7e904a8839dbaeab6c541ed08af8cc602c6
Sajjad1.0 <sajadehsas10@gmail.com>
2024-06-12 14:22:42 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       {/* Navvigation links include all pages in website */}
       <Navbar/>
 
-      <Routes basename='/jersey-sweden'>
+      <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/kits/:id' element={<Kits/>}/>
         <Route path='/product/:id' element={<Kits/>}/>
